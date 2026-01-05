d0b453d15003c77d4291a49dcccbd52faa7dfb72
sudhir002 <singh.sudhirkr111@gmail.com>
2026-01-05 17:18:52 +0530

    Use bcryptjs for cross-platform deploy

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const pool = require("../config/db");
 const { secret, expiresIn } = require("../config/jwt");
